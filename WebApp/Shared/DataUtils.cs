8d5910d0b50888f644e4ec3557d3009ca994a67a
EricCayeux <eric.cayeux@norceresearch.no>
2026-03-25 13:06:15 +0100

    incorporated depth references

--- WebApp/Shared/DataUtils.cs
+++ WebApp/Shared/DataUtils.cs
@@ -1,4 +1,7 @@
-﻿public static class DataUtils
+﻿using NORCE.Drilling.Well.ModelShared;
+using OSDC.UnitConversion.DrillingRazorMudComponents;
+
+public static class DataUtils
 {
     // default values
     public const double DEFAULT_VALUE = 999.25;
@@ -11,17 +14,62 @@
     public static class UnitAndReferenceParameters
     {
         public static string? UnitSystemName { get; set; } = "Metric";
-        public static string? DepthReferenceName { get; set; }
+        public static string? DepthReferenceName { get; set; } = "WGS84";
         public static string? PositionReferenceName { get; set; }
         public static string? AzimuthReferenceName { get; set; }
         public static string? PressureReferenceName { get; set; }
         public static string? DateReferenceName { get; set; }
+        public static GroundMudLineDepthReferenceSource GroundMudLineDepthReferenceSource { get; set; } = new GroundMudLineDepthReferenceSource();
+        public static SeaWaterLevelDepthReferenceSource SeaWaterLevelDepthReferenceSource { get; set; } = new SeaWaterLevelDepthReferenceSource();
     }
 
-    public static void UpdateUnitSystemName(string val)
+    public static void ApplyWellReferenceValues(Well? well, List<Cluster> clusters)
     {
-        UnitAndReferenceParameters.UnitSystemName = (string)val;
+        DataUtils.UnitAndReferenceParameters.GroundMudLineDepthReferenceSource.GroundMudLineDepthReference = 0;
+        DataUtils.UnitAndReferenceParameters.SeaWaterLevelDepthReferenceSource.SeaWaterLevelDepthReference = 0;
+        if (well != null && well.ClusterID != null)
+        {
+            Cluster? cluster = null;
+            foreach (var c in clusters)
+            {
+                if (c != null && c.MetaInfo != null && c.MetaInfo.ID == well.ClusterID)
+                {
+                    cluster = c;
+                    break;
+                }
+            }
+            if (cluster != null)
+            {
+                if (cluster.GroundMudLineDepth != null && cluster.GroundMudLineDepth.GaussianValue != null && cluster.GroundMudLineDepth.GaussianValue.Mean != null)
+                {
+                    ApplyGroundMudLineDepthWGS84(cluster.GroundMudLineDepth.GaussianValue.Mean);
+                }
+                if (cluster.TopWaterDepth != null && cluster.TopWaterDepth.GaussianValue != null && cluster.TopWaterDepth.GaussianValue.Mean != null)
+                {
+                    ApplyTopWaterDepthWGS84(cluster.TopWaterDepth.GaussianValue.Mean);
+                }
+            }
+        }
     }
+
+    public static void ApplyGroundMudLineDepthWGS84(double? val)
+    {
+        if (val != null)
+        {
+            DataUtils.UnitAndReferenceParameters.GroundMudLineDepthReferenceSource.GroundMudLineDepthReference = -val;
+        }
+    }
+
+    public static void ApplyTopWaterDepthWGS84(double? val)
+    {
+        if (val != null)
+        {
+            DataUtils.UnitAndReferenceParameters.SeaWaterLevelDepthReferenceSource.SeaWaterLevelDepthReference = -val;
+        }
+    }
+
+    public static void UpdateUnitSystemName(string value) => UnitAndReferenceParameters.UnitSystemName = value;
+    public static void UpdateDepthReferenceName(string value) => UnitAndReferenceParameters.DepthReferenceName = value;
 
     public static readonly string WellNameLabel = "Well Name";
     public static readonly string WellDescrLabel = "Well Description";
@@ -30,5 +78,16 @@
     public static readonly string DepthReferencesXValuesQty = "LengthStandard";
     public static readonly string DepthReferencesYValuesTitle = "Depth";
     public static readonly string DepthReferencesYValuesQty = "DepthDrilling";
+
+    public class GroundMudLineDepthReferenceSource : IGroundMudLineDepthReferenceSource
+    {
+        public double? GroundMudLineDepthReference { get; set; } = null;
+    }
+
+    public class SeaWaterLevelDepthReferenceSource : ISeaWaterLevelDepthReferenceSource
+    {
+        public double? SeaWaterLevelDepthReference { get; set; } = null;
+
+    }
 
 }
