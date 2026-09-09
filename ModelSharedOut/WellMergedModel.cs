3fd5e5626cc7be93daf0fb9643999f9888633e17
EricCayeux <eric.cayeux@norceresearch.no>
2026-09-09 13:07:32 +0200

    Contraction

--- ModelSharedOut/WellMergedModel.cs
+++ ModelSharedOut/WellMergedModel.cs
@@ -31479,14 +31479,6 @@ namespace OSDC.Drilling.Well.ModelShared
         [System.Text.Json.Serialization.JsonPropertyName("FixedPlatformProperties")]
         public FixedPlatformProperties FixedPlatformProperties { get; set; }
 
-        [System.Text.Json.Serialization.JsonPropertyName("DrillFloorElevation")]
-        [System.Obsolete]
-        public double? DrillFloorElevation { get; set; }
-
-        [System.Text.Json.Serialization.JsonPropertyName("IsFixedPlatform")]
-        [System.Obsolete]
-        public bool IsFixedPlatform { get; set; }
-
         [System.Text.Json.Serialization.JsonPropertyName("ClusterID")]
         public System.Guid? ClusterID { get; set; }
 
@@ -32114,9 +32106,6 @@ namespace OSDC.Drilling.Well.ModelShared
         [System.Text.Json.Serialization.JsonPropertyName("LastModificationDate")]
         public System.DateTimeOffset? LastModificationDate { get; set; }
 
-        [System.Text.Json.Serialization.JsonPropertyName("IsFixedPlatform")]
-        public bool IsFixedPlatform { get; set; }
-
         [System.Text.Json.Serialization.JsonPropertyName("ClusterID")]
         public System.Guid? ClusterID { get; set; }
 
@@ -32587,14 +32576,6 @@ namespace OSDC.Drilling.Well.ModelShared
 
         [System.Text.Json.Serialization.JsonPropertyName("FixedPlatformProperties")]
         public FixedPlatformProperties FixedPlatformProperties { get; set; }
-
-        [System.Text.Json.Serialization.JsonPropertyName("DrillFloorElevation")]
-        [System.Obsolete]
-        public double? DrillFloorElevation { get; set; }
-
-        [System.Text.Json.Serialization.JsonPropertyName("IsFixedPlatform")]
-        [System.Obsolete]
-        public bool IsFixedPlatform { get; set; }
 
         [System.Text.Json.Serialization.JsonPropertyName("ClusterID")]
         public System.Guid? ClusterID { get; set; }
